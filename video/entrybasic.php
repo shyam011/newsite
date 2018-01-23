0e7ca8e6a6ce5439effbd07d1bd2adc4af133bc9
Shyam <shyamsundar011@gmail.com>
2018-01-23 23:42:30 +0530

    basic

--- video/entrybasic.php
+++ video/entrybasic.php
@@ -1,5 +1,5 @@
 <?php
-include_once( "config.inc.php" );
+include_once( "/var/www/html/newsite/admin/config.inc.php" );
 include_once( SITE_DIR_ADMIN."include/main.function.php" );
 
 function __autoload($class_name) {
