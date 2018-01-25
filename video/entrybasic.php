ce63d03322acb5ff5ac5a8eb2597221c2e75b8c0
avya <shyamsundar011@gmail.com>
2018-01-25 19:41:34 +0530

    delete

--- video/entrybasic.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once( "/var/www/html/newsite/admin/config.inc.php" );
-include_once( SITE_DIR_ADMIN."include/main.function.php" );
-
-function __autoload($class_name) {
-	require_once("classes/$class_name.php");
-}
-?>
